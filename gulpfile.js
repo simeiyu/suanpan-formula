be8c5be89e308dbd10c4bfe98b4e57858dd89970
zhaomingxue <zhaomingxue@cn.fujitsu.com>
2020-03-12 10:19:58 +0800

    配置自动编译
    Signed-off-by: zhaomingxue <zhaomingxue@cn.fujitsu.com>

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ let revReplace = require('gulp-rev-replace');
 let del = require('del');
 let less = require('gulp-less');
 let cleanCSS = require('gulp-clean-css');
+let watch = require('gulp-watch');
+var browserSync = require('browser-sync').create(); 
 
 let paths = {
   template: ['./tpl/*.html'],
@@ -20,14 +22,37 @@ let paths = {
 };
 
 gulp.task('less', function () {
-  gulp.src(['./css/app.less'])
+  gulp.src(['./css/**'])
     .pipe(less())
     .pipe(gulp.dest('./css'));
 });
 
+gulp.task('html', function () {
+  gulp.src(['./tpl/*.html'])
+    .pipe(gulp.dest('dist/tpl/*.html'));
+});
+
+gulp.task('browser', function(){
+  browserSync.init({
+      server: './'    // 访问目录，自动指向该目录下的 index.html 文件
+  });
+});
+
+gulp.task('watch', function () {
+  w('./tpl/*.html', 'html');
+  w('./css/*.less', 'less');
+
+  function w(path, task){
+      watch(path, function () {
+          gulp.start(task);
+          browserSync.reload();
+      });
+  }
+});
 
 gulp.task('default', function () {
   gulp.run('less');
+  gulp.start(['html', 'browser', 'watch'])
   // gulp.watch(paths.template, ['templates']);
-  gulp.watch(paths.less, ['less']);
+  // gulp.watch(paths.less, ['less']);
 });
